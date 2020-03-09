efadd468242b5da7e74ab3aef9893b5f737a7508
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2020-03-09 21:10:23 +1100

    added html5 tags video, audio, and canvas

--- demo.js
+++ demo.js
@@ -8,6 +8,7 @@ function addToList() {
     list.style.backgroundColor = userInput;
     list.style.textTransform = "uppercase";
     list.style.listStyleType = "upper-roman";
+    list.style.borderRadius = "5px";
     if (document.getElementById("surprise")) {
         document.getElementById("surprise").classList.add("animation");
         document.getElementById("surprise").id = "sliding_spinning2";
@@ -27,6 +28,7 @@ function addToTopOfList() {
     list.style.color = userInput;
     list.style.textTransform = "lowercase";
     list.style.listStyleType = "lower-roman";
+    list.style.borderRadius = "20px";
     if (document.getElementById("surprise")) {
         document.getElementById("surprise").classList.add("animation");
         document.getElementById("surprise").id = "bouncing2";
@@ -34,4 +36,35 @@ function addToTopOfList() {
     if (document.getElementById("sliding_spinning2")) {
         document.getElementById("sliding_spinning2").id = "bouncing2";
     }
+}
+
+function drawRectangle() {
+    var canvas = document.getElementById("canvasDemo");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "blue";
+    ctx.fillRect(50, 25, 100, 75);
+}
+
+function drawTriangle1() {
+    var canvas = document.getElementById("canvasDemo");
+    var ctx = canvas.getContext("2d");
+    var userInput = document.getElementById("color").value;
+    ctx.fillStyle = userInput;
+    ctx.beginPath();
+    ctx.moveTo(50, 25);
+    ctx.lineTo(150, 100);
+    ctx.lineTo(50, 100);
+    ctx.fill();
+}
+
+function drawTriangle2() {
+    var canvas = document.getElementById("canvasDemo");
+    var ctx = canvas.getContext("2d");
+    var userInput = document.getElementById("color").value;
+    ctx.fillStyle = userInput;
+    ctx.beginPath();
+    ctx.moveTo(50, 25);
+    ctx.lineTo(150, 100);
+    ctx.lineTo(150, 25);
+    ctx.fill();
 }
