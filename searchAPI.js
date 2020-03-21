689916ac5e4eb7ddc2bdf410cef3c67a418c2c7b
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2020-03-21 14:43:20 +1100

    added radio buttons to booklist display

--- searchAPI.js
+++ searchAPI.js
@@ -4,6 +4,27 @@ var searchBy = "";
 var searchText = "";
 var input = "";
 
+class DetailedBook {
+    constructor(title, author, id, publisher, publishedDate, pageCount, imageURL, description) {
+        this.title = title;
+        this.author = author;
+        this.id = id;
+        this.publisher = publisher;
+        this.publishedDate = publishedDate;
+        this.pageCount = pageCount;
+        this.imageURL = imageURL;
+        this.description = description;
+    }
+}
+
+class Book {
+    constructor(title, author, id) {
+        this.title = title;
+        this.author = author;
+        this.id = id;
+    }
+}
+
 
 function getBooksByTitle() {
     var xmlhttp = new XMLHttpRequest();
@@ -47,16 +68,61 @@ function getBooksByAuthor() {
 
 
 function myDisplayFunction(result) {
+    var disp = document.getElementById("booklist");
+    var prev = disp.children[0];
+    var booklist = "Here are the first 20 books from your search: <br><br>";
+    var books = new Array();
+    for(var i=0; i < result.items.length; i++) {
+        console.log(result.items[i]);
+        var elem;
+
+        var title = result.items[i].volumeInfo.title;
+        var author = result.items[i].volumeInfo.authors; 
+        var id = result.items[i].id;
+
+        var book = new Book(title, author, id); 
+        
+        books[i] = book;
+
+        console.log(books[i].title);
+
+        elem = document.createElement("br");
+        disp.insertBefore(elem, prev);
+        prev = elem;
+
+        elem = document.createTextNode(title + ", by " + author);
+        disp.insertBefore(elem, prev);
+        prev = elem;
+
+        elem = document.createElement("input");
+        elem.type = "radio";
+        elem.name = "book[]";
+        elem.id = i;
+        elem.value = id;
+        disp.insertBefore(elem, prev);
+        prev = elem;
+
+    }
+    document.getElementById("output").innerHTML = booklist;
+}
+
+
+
+function myDisplayFunction2(result) {
     console.log(result.items[0]);
     var booklist = "Here are the first 20 books from your search: <br><br>";
 
     for(var i=0; i < result.items.length; i++) {
         var title = result.items[i].volumeInfo.title;
-        console.log(title);
-        var author = result.items[i].volumeInfo.authors;
-        console.log(author);  
+        var author = result.items[i].volumeInfo.authors[0]; 
         var id = result.items[i].id;
-        console.log(id);
+        var publisher = result.items[i].publisher;
+        var publishedDate = result.items[i].publishedDate;
+        var pageCount = result.items[0].volumeInfo.pageCount;
+        var imageURL = "";
+        if (result.items[0].volumeInfo.imageLinks.thumbnail) {
+            imageURL = result.items[0].volumeInfo.imageLinks.thumbnail;
+        } 
         booklist += '<strong>' + title + '</strong> by ' + author + '<br>';
     }
     document.getElementById("output").innerHTML = booklist;
